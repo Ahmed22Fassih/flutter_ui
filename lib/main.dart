dc15ed16e4a179f9d8cfa9b310df1fde526880c1
Samir-PC <ahmed.fassih22@gmail.com>
2020-09-28 07:39:33 +0200

    sec commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  MyHomePage(),
+      home: new MyHomePage(),
     );
   }
 }
